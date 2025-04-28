a8a8314897e42ad499baf285849b1b6879a837ec
root <root@endmist.aeza.network>
2025-04-28 19:25:44 +0000

    Fix: Remove unused USE_MOCK_DATA constant

--- src/BookService.js
+++ src/BookService.js
@@ -1,24 +1,10 @@
 import axios from 'axios';
 
-// ???????, ??? USE_MOCK_DATA ?????????? ? false
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const USE_MOCK_DATA = false; // Оставляем, если вдруг используется где-то еще
+// Закомментируем или удалим неиспользуемую константу
+// const USE_MOCK_DATA = false;
 
 // URL ???????
 const API_URL = 'http://89.208.104.134:8080/api/books';
-
-// УДАЛЕНО: Неиспользуемая функция isApiAvailable
-// const isApiAvailable = async () => {
-//   if (USE_MOCK_DATA) return false;
-
-//   try {
-//     await axios.get(`${API_URL}/health`, { timeout: 2000 });
-//     return true;
-//   } catch (error) {
-//     console.warn('API ??????????, ????????? ??????? ??????');
-//     return false;
-//   }
-// };
 
 // ???????? ??????? API
 const realGetAllBooks = () => axios.get(API_URL);
@@ -26,17 +12,16 @@ const realGetBookById = (id) => axios.get(`${API_URL}/${id}`);
 const realCreateBook = (book) => axios.post(API_URL, book);
 const realUpdateBook = (id, book) => axios.put(`${API_URL}/${id}`, book);
 const realDeleteBook = (id) => axios.delete(`${API_URL}/${id}`);
-const realSearchBooks = (query) => axios.get(`${API_URL}/search?query=${encodeURIComponent(query)}`); // Добавил encodeURIComponent
+const realSearchBooks = (query) => axios.get(`${API_URL}/search?query=${encodeURIComponent(query)}`);
 
 // ???????????? ??????
 const BookService = {
-  getAllBooks: () => realGetAllBooks(),
-  getBookById: (id) => realGetBookById(id),
-  createBook: (book) => realCreateBook(book),
-  updateBook: (id, book) => realUpdateBook(id, book),
-  deleteBook: (id) => realDeleteBook(id),
-  searchBooks: (query) => realSearchBooks(query),
-  // isApiAvailable: isApiAvailable // УДАЛЕНО
+  getAllBooks: realGetAllBooks, // Можно просто присвоить функции
+  getBookById: realGetBookById,
+  createBook: realCreateBook,
+  updateBook: realUpdateBook,
+  deleteBook: realDeleteBook,
+  searchBooks: realSearchBooks,
 };
 
 export default BookService;
